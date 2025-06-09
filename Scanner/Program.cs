114fc32c3022140c6c6d21cb1bbbd90cdf9549de
josago97 <josago2009@hotmail.es>
2025-06-09 17:00:48 +0200

    Fix read json scanner

--- Scanner/Program.cs
+++ Scanner/Program.cs
@@ -26,7 +26,7 @@ internal class Program
         fileName = Path.ChangeExtension(fileName, "json");
         string filePath = Path.Combine(Properties.INPUT_PATH, fileName);
         Console.WriteLine("Read Json: " + filePath);
-        string json = File.ReadAllText(fileName);
+        string json = File.ReadAllText(filePath);
 
         return JsonSerializer.Deserialize<T>(json, JsonSerializerOptions.Web);
     }
